2f263ec39f63ab20f23af48f5fe866fd72d348d1
hotfireeagle <hotfireeagle@163.com>
2019-02-26 18:02:36 +0800

    完善readme

--- app/common/js/common.js
+++ app/common/js/common.js
@@ -12,19 +12,44 @@ $('.mi').on('click', function() {
 });
 
 /** 取本地的cookie，用户名以及登录信息都存在本地cookie中 */
-function getCookie(key) {
-    let cacheobj = {};                      // 缓存保存，避免每次都进行遍历
+var cacheobj = {};                      // 缓存保存，避免每次都进行遍历
+function getCookie(keyName) {
     let result = '';
     let cookies = document.cookie || '';
+    if (keyName in cacheobj) {
+        return cacheobj[keyName];
+    }
     cookies = cookies.split(';') || [];
-    cookies.map(keyValue => {
+    for (let i = 0, len = cookies.length; i < len; i++) {
+        let keyValue = cookies[i];
         let arr = keyValue && keyValue.split('=') || [];
-        let key = arr[0] || '';
+        let key = (arr[0] && arr[0].trim()) || '';
         let value = arr[1] || '';
-        
-    });
+        if (!cacheobj[key] || cacheobj[key] != value) {
+            cacheobj[key] = value;
+        }
+        if (keyName === key) {
+            result = value;
+            break;
+        }
+    }
+    return result;
 }
 
+/** 控制在顶部菜单是显示名字还是注册登录 */
+function displayNameOrLogin() {
+    let userName = getCookie('name');
+    if (userName) {
+        $('.rl').hide();
+        $('#userName').text(userName);
+        $('#userName').show();
+    } else {
+        $('.rl').show();
+        $('#userName').hide();
+    }
+}
+
+displayNameOrLogin();
 
 /** 设置cookie */
 function setCookie(key, value) {
