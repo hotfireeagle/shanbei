a531c6f7101c2c4eda913c383371fbd8f769bee8
hotfireeagle <hotfireeagle@163.com>
2019-02-27 20:59:23 +0800

    取消字体渐变

--- app/biz/register/index.js
+++ app/biz/register/index.js
@@ -4,13 +4,19 @@ import 'app/common/js/common.js';
 let baseOrigin = '/EnglishLearningPlatform';
 
 $(document).ready(function() {
-    /** 关闭弹窗 */
+    /** 关闭消息提示弹窗 */
     $('.message .close')
     .on('click', function() {
         $(this)
         .closest('.message')
         .transition('fade');
     });
+
+    /** 关闭modal的提示 */
+    $('#closeButton').on('click', function() {
+        $('.small.modal').modal('hide');
+    });
+    
     $('#submit').on('click', function() {
         let userId = $('#userId').val();
         let userPwd = $('#userPwd').val();
@@ -32,6 +38,8 @@ $(document).ready(function() {
                         $('.message').hide();                  // 显示2.5s后进行隐藏
                         window.location.href = '/login.html';
                     }, 1500);
+                } else {
+                    $('.small.modal').modal('show');
                 }
                 $(that).removeClass('loading disabled');
             }
