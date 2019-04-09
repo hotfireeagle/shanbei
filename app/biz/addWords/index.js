155af3b8ca8ec7085bc56099a31a3ab6a0bcc40a
hotfireeagle <hotfireeagle@163.com>
2019-04-10 01:34:45 +0800

    增加单词

---
+++ app/biz/addWords/index.js
@@ -0,0 +1,81 @@
+import './index.less';
+import 'app/common/js/common.js';
+
+let baseOrigin = '/EnglishLearningPlatform';
+
+let globalConst = {
+    wordType: '1',
+};
+
+
+/** 监听点击按钮添加单词 */
+function addWords () {
+    $('#addWord').click(function () {
+        
+        let wordEnglish = $('.english').val();
+        let wordChinese = $('.chinese').val();
+        let wordType = globalConst.wordType;
+
+        if (!checkisLegal(wordEnglish, wordChinese)) return;
+
+        let data = { wordChinese, wordEnglish, wordType };
+        sendXHR(data);
+
+    });
+}
+
+/** 清空输入框中的单词 */
+function clearInput () {
+    $('.english').val('');
+    $('.chinese').val('');
+}
+
+
+/** 检测是否输入有效的英文单词以及中文释义 */
+function checkisLegal (english, chinese) {
+    if (!english || !chinese) {
+        $('#message').text('请输入有效的英文单词以及中文释义');
+        $('.small.modal').modal('show');
+        return false;
+    }
+    return true;
+}
+
+/** 发送xhr数据 */
+function sendXHR (data) {
+    $.ajax({
+        url: `${baseOrigin}/essayController/addEssay`,
+        type: "POST",
+        data: JSON.stringify(data),
+        headers: {
+            'content-type': 'application/json',
+            'accept': 'application/json'
+        },
+        success: function(data, textStatus) {
+            if (data.RTNDESC == '成功') {
+                clearInput();
+                $('#message').text('更新成功');
+                $('.small.modal').modal('show');
+            } else {
+                $('.small.modal').modal('show');
+            }
+        }
+    });
+}
+
+
+$(document).ready(function () {
+    addWords();
+
+    /** 监听点击模态的关闭按钮 */
+    $('#closeButton').click(function () {
+        $('.small.modal').modal('hide');
+    });
+
+    $('.ui.dropdown')
+    .dropdown({
+        onChange: function (value) {
+            globalConst.wordType = ''+value;
+        }
+    });
+});
